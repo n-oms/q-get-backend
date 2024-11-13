b848d11bff0cfc2fddb93eb44300fd350e2c0e04
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2024-11-13 22:07:05 +0530

    scans patch

--- src/handlers/users/getme/handler.ts
+++ src/handlers/users/getme/handler.ts
@@ -4,40 +4,43 @@ import { Operations } from "@/libs/enums/common";
 import { users } from "@/libs/services/mongo/models/user";
 
 export class GetUserHandler implements IHandler {
-    operation: Operations;
-    isIdempotent: boolean;
-    operationId: string;
-    resource: string;
-    validations: any[];
-    isAuthorizedAccess: boolean;
+  operation: Operations;
+  isIdempotent: boolean;
+  operationId: string;
+  resource: string;
+  validations: any[];
+  isAuthorizedAccess: boolean;
 
-    constructor() {
-        this.operation = Operations.READ;
-        this.isIdempotent = true;
-        this.operationId = "getUser";
-        this.resource = "getMe";
-        this.validations = [];
-        this.isAuthorizedAccess = true;
+  constructor() {
+    this.operation = Operations.READ;
+    this.isIdempotent = true;
+    this.operationId = "getUser";
+    this.resource = "getMe";
+    this.validations = [];
+    this.isAuthorizedAccess = true;
+  }
+
+  public async handler(
+    req: ApiRequest,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> {
+    const { phoneNumber } = req.userInfo;
+
+    if (!phoneNumber) {
+      return res.status(400).json({ message: "Phone number is required" });
     }
 
-    public async handler(req: ApiRequest, res: Response, next: NextFunction): Promise<Response> {
-        const { phoneNumber } = req.userInfo;
+    try {
+      const user = await users.findOne({ phoneNumber }).exec();
 
-        if (!phoneNumber) {
-            return res.status(400).json({ message: "Phone number is required" });
-        }
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
 
-        try {
-            const user = await users.findOne({ phoneNumber }).exec();
-
-            if (!user) {
-                return res.status(404).json({ message: "User not found" });
-            }
-
-
-            return res.status(200).json(user);
-        } catch (error) {
-            next(error)
-        }
+      return res.status(200).json(user);
+    } catch (error) {
+      next(error);
     }
+  }
 }
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -7,45 +7,49 @@ import { IHandler } from "@/libs/types/common";
 import { validate } from "@/libs/validations/api";
 import { authorizeRequest } from "@/middlewares/authorizer/authorizer";
 import { Express } from "express";
-import {MobileVersionHandler} from '@/handlers/mobile-version/get-mobile-version/handler';
+import { MobileVersionHandler } from "@/handlers/mobile-version/get-mobile-version/handler";
 import { GetApplicationsDataHandler } from "@/handlers/applications/get/handler";
-import { GetScanHandler } from "@/handlers/scans/get/handler";
-import { GetUserHandler } from "@/handlers/users/getMe/handler";
 import { CheckAuthorizationHandler } from "@/handlers/authorization/check-authorization/handler";
 import { InvoiceApiPostHandler } from "@/handlers/invoice/post/handler";
-
+import { GetScanHandler } from "@/handlers/scans/get/handler";
+import { GetUserHandler } from "@/handlers/users/getMe/handler";
 
 const MAP_KEY_PAIR = [
-    [Operations.CREATE, HTTP_METHODS.POST], [Operations.REPLACE, HTTP_METHODS.PUT],
-    [Operations.DELETE, HTTP_METHODS.DELETE], [Operations.UPDATE, HTTP_METHODS.PATCH],
-    [Operations.INVOKE, HTTP_METHODS.POST], [Operations.READ, HTTP_METHODS.GET]
+  [Operations.CREATE, HTTP_METHODS.POST],
+  [Operations.REPLACE, HTTP_METHODS.PUT],
+  [Operations.DELETE, HTTP_METHODS.DELETE],
+  [Operations.UPDATE, HTTP_METHODS.PATCH],
+  [Operations.INVOKE, HTTP_METHODS.POST],
+  [Operations.READ, HTTP_METHODS.GET],
 ];
 const HTTP_OPERATION_MAP = new Map(MAP_KEY_PAIR as any);
 const API_VERSION = process.env.API_VERSION || APP_CONSTANTS.APP_VERSION;
 export const registerRoutes = function (app: Express) {
-
-    const routeHandlers: Array<IHandler> = getAllRouteHandlers();
-    routeHandlers.forEach(element => {
-        const httpMethod = HTTP_OPERATION_MAP.get(element.operation) as string;
-        const relativePath = `/${API_VERSION}/${element.resource}`;
-        app[httpMethod](
-            relativePath,
-            element.isAuthorizedAccess ? authorizeRequest : (req, res, next) => next(),
-            validate(element.validations), element.handler);
-    });
-    app.use(errorHandler);
-    
-}
+  const routeHandlers: Array<IHandler> = getAllRouteHandlers();
+  routeHandlers.forEach((element) => {
+    const httpMethod = HTTP_OPERATION_MAP.get(element.operation) as string;
+    const relativePath = `/${API_VERSION}/${element.resource}`;
+    app[httpMethod](
+      relativePath,
+      element.isAuthorizedAccess
+        ? authorizeRequest
+        : (req, res, next) => next(),
+      validate(element.validations),
+      element.handler
+    );
+  });
+  app.use(errorHandler);
+};
 
 function getAllRouteHandlers(): Array<IHandler> {
-    const routeHandlers: Array<IHandler> = [];
-    routeHandlers.push(new OtpApiHandler());
-    routeHandlers.push(new GetDashboardDataHandler());
-    routeHandlers.push(new MobileVersionHandler());
-    routeHandlers.push(new GetApplicationsDataHandler());
-    routeHandlers.push(new CheckAuthorizationHandler()); 
-    routeHandlers.push(new GetScanHandler());
-    routeHandlers.push(new GetUserHandler());
-    routeHandlers.push(new InvoiceApiPostHandler());
-    return routeHandlers;
+  const routeHandlers: Array<IHandler> = [];
+  routeHandlers.push(new OtpApiHandler());
+  routeHandlers.push(new MobileVersionHandler());
+  routeHandlers.push(new CheckAuthorizationHandler());
+  routeHandlers.push(new InvoiceApiPostHandler());
+  routeHandlers.push(new GetScanHandler());
+  routeHandlers.push(new GetDashboardDataHandler());
+  routeHandlers.push(new GetApplicationsDataHandler());
+  routeHandlers.push(new GetUserHandler());
+  return routeHandlers;
 }
